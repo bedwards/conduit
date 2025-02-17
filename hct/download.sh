3488e7af620d30f6c1c452766f3459bee45adda0
Brian Edwards <brian.mabry.edwards@gmail.com>
2025-02-17 08:49:15 -0500

    fix the download script

--- hct/download.sh
+++ hct/download.sh
@@ -6,10 +6,16 @@ mkdir -p ./input/hct-conduit
 mkdir -p ./working/csv
 
 c="equity-post-HCT-survival-predictions"
-csv="./input/equity-post-HCT-survival-predictions"
+csv="./input/$c"
 mkdir -p $csv
 kaggle c download $c -p $csv
+unzip -o ${csv}/*.zip -d $csv
+rm -fv ${csv}/*.zip
 
 lb="./input/hct-leaderboard"
 mkdir -p $lb
+rm -fv ${lb}/*.csv
 kaggle c leaderboard $c -d -p $lb
+unzip -o "$lb"/*.zip -d $lb
+rm -fv ${lb}/*.zip
+mv -fv ${lb}/*.csv ${lb}/lb-`date +%Y-%m-%d`.csv
