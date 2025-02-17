ffb41b896190562555a42c5da953a61ada325a91
Brian Edwards <brian.mabry.edwards@gmail.com>
2025-02-17 07:59:27 -0500

    download csv files

---
+++ hct/download.sh
@@ -0,0 +1,14 @@
+#!/bin/sh
+
+set -eux
+
+mkdir -p ./input/hct-conduit
+
+c="equity-post-HCT-survival-predictions"
+csv="./input/equity-post-HCT-survival-predictions"
+mkdir -p $csv
+kaggle c download $c -p $csv
+
+lb="./input/hct-leaderboard"
+mkdir -p $lb
+kaggle c leaderboard $c -d -p $lb
