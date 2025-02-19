b16be6ca2ac550c29c639d87de39fe78c70082e5
Brian Edwards <brian.mabry.edwards@gmail.com>
2025-02-19 11:15:51 -0500

    hct conduit-01 improved

--- hct/working/conduit-01.py
+++ hct/working/conduit-01.py
@@ -4,11 +4,11 @@ import warnings; warnings.simplefilter('ignore')
 import os
 import sys
 import csv
+import contextlib
 from glob import glob
 from functools import partial
-from multiprocessing import Pool
-from itertools import combinations
 from collections import defaultdict
+from itertools import combinations, starmap
 import numpy as np
 import pandas as pd
 import xgboost as xgb
@@ -21,29 +21,57 @@ from lifelines.utils import concordance_index
 import joblib
 
 NAME = 'conduit-01'
+
+# if True: submission.csv created using all models from PIPE
+# if False: uses ensemble combination with best CV score
+SUBMIT_FULL_ENSEMBLE = True
+
+INCLUDE_FIT_ON_KAGGLE = True
+
+PROVE_MY_SCORE_IS_SAME = False
+
 C_PATH = '../input/equity-post-HCT-survival-predictions'
 LB_PATH = '../input/hct-leaderboard'
 CSV_PATH = f'./csv/{NAME}'
-MODEL_PATH = '../input/hct-conduit'
-
 os.makedirs(CSV_PATH, exist_ok=True)
 
-# https://www.kaggle.com/code/cdeotte/gpu-lightgbm-baseline-cv-681-lb-685
-kwargs_lgb = dict(max_depth=3,
-                  colsample_bytree=0.4,
-                  n_estimators=2500,
-                  learning_rate=0.02,
-                  num_leaves=8)
+MODEL_PATH = '../input/hct-conduit'
 
+try:
+    get_ipython
+except NameError:
+    RUNNING_ON_KAGGLE = False
+else:
+    RUNNING_ON_KAGGLE = True
+
+if RUNNING_ON_KAGGLE:
+    if INCLUDE_FIT_ON_KAGGLE:
+        MODEL_PATH = f'./models/{NAME}'
+        os.makedirs(MODEL_PATH, exist_ok=True)
+else:
+    os.makedirs(MODEL_PATH, exist_ok=True)
+
+# https://www.kaggle.com/code/cdeotte/gpu-lightgbm-baseline-cv-681-lb-685
 kwargs_xgb = dict(max_depth=3,
                   colsample_bytree=0.5,
                   subsample=0.8,
                   n_estimators=2000,
                   learning_rate=0.02,
-                  min_child_weight=80)
+                  min_child_weight=80,
+                  enable_categorical=True,
+                  verbosity=0)
 
 kwargs_cb = dict(learning_rate=0.1,
-                 grow_policy='Lossguide')
+                 grow_policy='Lossguide',
+                 silent=True)
+
+kwargs_lgb = dict(max_depth=3,
+                  colsample_bytree=0.4,
+                  n_estimators=2500,
+                  learning_rate=0.02,
+                  num_leaves=8,
+                  verbose=-1,
+                  verbosity=-1)
 
 PIPES = [
     {
@@ -73,6 +101,19 @@ PIPES = [
         'kwargs': dict(loss_function="Cox", iterations=400, use_best_model=False, **kwargs_cb),
     }
 ]
+
+if RUNNING_ON_KAGGLE:
+    for pipe in PIPES:
+        if pipe['m'] in ['xgb', 'xgb_cox']:
+            pipe['kwargs']= dict(device='cuda', **pipe['kwargs'])
+            continue
+
+        if pipe['m'] == 'cb':
+            pipe['kwargs'] = dict(task_type='GPU', **pipe['kwargs'])
+            continue
+
+        if pipe['m'] == 'lgb':
+            pipe['kwargs'] = dict(device='gpu', **pipe['kwargs'])
 
 
 def read_csv(path):
@@ -164,6 +205,7 @@ def fit_fold_model(X, y, fold_n, i_fold, i_oof, m_name, m):
         fit_kwargs['verbose'] = False
 
     m.fit(X.iloc[i_fold], y.iloc[i_fold], **fit_kwargs)
+
     filename = f'{MODEL_PATH}/{NAME}-{m_name}-{fold_n}.joblib'
     joblib.dump(m, filename)
     print(f'fold {fold_n} wrote {filename}')
@@ -176,9 +218,9 @@ def fit():
     kfold = create_kfold()
 
     m_constructors = {
-        'xgb': partial(xgb.XGBRegressor, enable_categorical=True),
-        'lgb': partial(lgb.LGBMRegressor, verbose=-1),
-        'cb': partial(cb.CatBoostRegressor, silent=True),
+        'xgb': xgb.XGBRegressor,
+        'lgb': lgb.LGBMRegressor,
+        'cb': cb.CatBoostRegressor,
     }
 
     models = []
@@ -187,6 +229,7 @@ def fit():
         Model = m_constructors[pipe['m'].split('_')[0]]
         if pipe['m'].startswith('cb') and pipe['X'] in ['raw', 'label']:
             pipe['kwargs']['cat_features'] = Xs[pipe['X']].select_dtypes('category').columns.to_list()
+
         models.append((
             pipe['m'],
             Model(**pipe['kwargs']),
@@ -201,20 +244,28 @@ def fit():
         for m_name, m, X, y in models:
             args.append((X, y, fold_n, i_fold, i_oof, m_name, m))
 
-    with Pool(min(os.cpu_count(), kfold.get_n_splits() * len(models))) as pool:
-        pool.starmap(fit_fold_model, args)
+    if RUNNING_ON_KAGGLE:
+        list(starmap(fit_fold_model, args))
+    else:
+        from multiprocessing import Pool
+        with Pool(min(os.cpu_count(), kfold.get_n_splits() * len(models))) as pool:
+            pool.starmap(fit_fold_model, args)
 
 
 def calc_score(train, y_pred_oof):
     merged_df = train[['race_group', 'efs_time', 'efs']].assign(prediction=y_pred_oof)
-    merged_df.reset_index(inplace=True)
+    merged_df = merged_df.reset_index()
     merged_df_race_dict = dict(merged_df.groupby(['race_group']).groups)
     metric_list = []
 
     for race in merged_df_race_dict.keys():
         indices = sorted(merged_df_race_dict[race])
         merged_df_race = merged_df.iloc[indices]
-        c_index_race = concordance_index(merged_df_race['efs_time'], -merged_df_race['prediction'], merged_df_race['efs'])
+
+        c_index_race = concordance_index(merged_df_race['efs_time'],
+                                         -merged_df_race['prediction'],
+                                         merged_df_race['efs'])
+
         metric_list.append(c_index_race)
 
     return float(np.mean(metric_list)-np.sqrt(np.var(metric_list)))
@@ -250,9 +301,9 @@ def cv_score():
 
     for r in range(1, len(y_pred_oofs)+1):
         for m_combo in combinations(y_pred_oofs.items(), r):
-            m_names, ranked_risk_score = zip(*m_combo)
+            m_names, ranked_risk_scores = zip(*m_combo)
             m_names = '-'.join(m_names)
-            score = calc_score(train, sum(ranked_risk_score))
+            score = calc_score(train, sum(ranked_risk_scores))
             lb_pct = leaderboard_percentile(score)
             scores.append((score, lb_pct, m_names))
 
@@ -268,18 +319,50 @@ def cv_score():
     for score, lb_pct, m_names in scores:
         print(f'{score:.4f} {lb_pct:.4f} {m_names}')
 
+    if PROVE_MY_SCORE_IS_SAME:
+        print('-'*79)
+        import metric
+        solution = train[['race_group', 'efs', 'efs_time']].reset_index()
+        row_id_column_name = 'ID'
+        scores = []
+
+        for r in range(1, len(y_pred_oofs)+1):
+            for m_combo in combinations(y_pred_oofs.items(), r):
+                m_names, ranked_risk_scores = zip(*m_combo)
+                m_names = '-'.join(m_names)
+                submission = pd.DataFrame(sum(ranked_risk_scores), index=train.index, columns=['prediction']).reset_index()
+                score = metric.score(solution.copy(), submission.copy(), row_id_column_name)
+                lb_pct = leaderboard_percentile(score)
+                scores.append((score, lb_pct, m_names))
+
+        scores = sorted(scores)
+
+        for score, lb_pct, m_names in scores:
+            print(f'{score:.4f} {lb_pct:.4f} {m_names}')
 
 def predict():
     print('\npredict')
 
     fn = f'{CSV_PATH}/scores.csv'
+    row_models_len_max = 0
 
     with open(fn, newline='') as f:
         for row in csv.DictReader(f):
-            cv_score = float(row['score'])
-            m_names = row['models'].split('-')
+            values = lambda: (float(row['score']),
+                              float(row['leaderboard_percentile']),
+                              row['models'].split('-'))
 
-    print(f'using ensemble with best CV score: {cv_score:.4f} {m_names}')
+            if not SUBMIT_FULL_ENSEMBLE:
+                cv_score, lb_pct, m_names = values()
+                continue
+
+            if row_models_len_max < len(row['models']):
+                cv_score, lb_pct, m_names = values()
+                m_names = row['models'].split('-')
+
+    msg = 'full ensemble' if SUBMIT_FULL_ENSEMBLE else 'ensemble with best CV score'
+    print(f'using {msg}: {cv_score:.4f} {lb_pct:.4f} {m_names}')
+
     Xs = get_Xs('test')
     m_name_Xs = []
 
@@ -308,37 +391,46 @@ def predict():
 
 
 if __name__ == '__main__':
-    try:
-        get_ipython
+    if not RUNNING_ON_KAGGLE:
+        if sys.argv[1:] and sys.argv[1] == 'preprocess':
+            preprocess()
+            sys.exit()
 
-    except NameError:
-        pass
+        if sys.argv[1:] and sys.argv[1] == 'cv_score':
+            cv_score()
+            sys.exit()
 
-    else:
-        preprocess()  # kaggle notebook
-        cv_score()
-        predict()
-        sys.exit()
+        if sys.argv[1:] and sys.argv[1] == 'predict':
+            predict()
+            sys.exit()
 
-    # terminal
-    os.makedirs(MODEL_PATH, exist_ok=True)
+        for fn in glob(f'{CSV_PATH}/*.csv'):
+            os.remove(fn)
 
-    if sys.argv[1:] and sys.argv[1] == 'preprocess':
+        for fn in glob(f'{MODEL_PATH}/*.joblib'):
+            os.remove(fn)
+
         preprocess()
-        sys.exit()
-
-    if sys.argv[1:] and sys.argv[1] == 'cv_score':
+        fit()
         cv_score()
-        sys.exit()
-
-    if sys.argv[1:] and sys.argv[1] == 'predict':
         predict()
         sys.exit()
+
+    # running on kaggle
+    preprocess()
+    if INCLUDE_FIT_ON_KAGGLE:
+        fit()
+    cv_score()
+    predict()
+
+    for fn in glob(f'{CSV_PATH}/*.csv'):
+        os.remove(fn)
 
     for fn in glob(f'{MODEL_PATH}/*.joblib'):
         os.remove(fn)
 
-    preprocess()
-    fit()
-    cv_score()
-    predict()
+    for fn in glob(f'./catboost_info/**'):
+        os.remove(fn)
+
+    for path in [CSV_PATH, MODEL_PATH, 'catboost_info']:
+        os.rmdir(path)
